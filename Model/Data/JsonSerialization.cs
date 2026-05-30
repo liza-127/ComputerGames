5ae655de9c2ad2f502e441d43cd0c027fa060f26
liza-127 <lizadaystar12737@gmail.com>
2026-05-30 18:12:08 +0300

    Update JsonSerialization.cs

--- Model/Data/JsonSerialization.cs
+++ Model/Data/JsonSerialization.cs
@@ -1,8 +1,11 @@
-﻿using Model.Core;
-using Newtonsoft.Json.Linq;
+using Model.Core;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using Newtonsoft.Json.Linq;
 
 namespace Model.Data
 {
@@ -63,6 +66,55 @@ namespace Model.Data
             }
 
             return games;
+        }
+
+        public void Serializeone(string filePath, Game game)
+        {
+            string file = Path.Combine(filePath, game.Title + ".json");
+            JObject jsonGame = new JObject
+            {
+                { "Title", game.Title },
+                { "Genre", game.Genre },
+                { "AgeRating", game.AgeRating },
+                { "ReleaseDate", game.ReleaseDate.ToString("dd-MM-yyyy") },
+                { "Rating", game.Rating },
+                { "ImageURL", game.ImageURL },
+                { "Type", game.GameMode }
+            }   ;
+
+            File.WriteAllText(file, jsonGame.ToString());
+        }
+        public Game Deserializeone(string filePath)
+        {
+
+            if (!File.Exists(filePath))
+                return null;
+
+            string jsonText = File.ReadAllText(filePath);
+            JObject jsonGame = JObject.Parse(jsonText);
+
+            string title = jsonGame["Title"]?.ToString();
+            string genre = jsonGame["Genre"]?.ToString();
+            int ageRating = int.Parse(jsonGame["AgeRating"]?.ToString() ?? "0");
+            DateTime releaseDate = DateTime.Parse(jsonGame["ReleaseDate"]?.ToString() ?? DateTime.MinValue.ToString());
+            double rating = double.Parse(jsonGame["Rating"]?.ToString() ?? "0");
+            string imageURL = jsonGame["ImageURL"]?.ToString();
+            string type = jsonGame["Type"]?.ToString();
+
+            if (type == "Single")
+            {
+                return new SingleGame(title, genre, ageRating, releaseDate, rating, imageURL);
+            }
+            else if (type == "Multiplayer")
+            {
+                return new MultiplayerGame(title, genre, ageRating, releaseDate, rating, imageURL);
+            }
+            else if (type == "Online")
+            {
+                return new OnlineGame(title, genre, ageRating, releaseDate, rating, imageURL);
+            }
+
+            return null;
         }
     }
 }
